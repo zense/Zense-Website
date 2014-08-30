6092a60f41adde88bc6a08d80d0f7b14074d8bd4
bobo <vikasyadavgood@gmail.com>
2014-08-30 22:29:53 +0530

    Modified

--- portfolio.php
+++ portfolio.php
@@ -76,6 +76,7 @@ $test =2;
                     <div class="slide-text">
                         <h1> So what have we been up to?</h1>
                         <h2>Our community has a bunch of cool projects and we have many more to come. Spend some time here and check out our work :)</h2>
+                        
                     </div>
                     <a target="blank" href="https://www.facebook.com/zense.dev"><img src="images/home/slider/like.png" class="slider-hill" alt="slider image" style="width:400px;height:auto"></a>
                 </div>
@@ -92,6 +93,23 @@ $test =2;
                 <h1>Completed Projects</h1> 
                 <br>
                 <div class="portfolio-items">
+					<div class="col-xs-6 col-sm-4 col-md-3 portfolio-item branded logos">
+                        <div class="portfolio-wrapper">
+                            <div class="portfolio-single">
+                                <div class="portfolio-thumb">
+                                    <img src="images/portfolio/6.png" class="img-responsive" alt="">
+                                </div>
+                                <div class="portfolio-view">
+                                    <ul class="nav nav-pills">
+                                        <li><a data-toggle="modal" data-target="#modal6"><i class="fa fa-link"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="portfolio-info ">
+                                <h2>Connect the Disconnected</h2>
+                            </div>
+                        </div>
+                    </div>
                     <div class="col-xs-6 col-sm-4 col-md-3 portfolio-item branded logos">
                         <div class="portfolio-wrapper">
                             <div class="portfolio-single">
@@ -261,7 +279,8 @@ $test =2;
                             </div>
                             <div class="project-info overflow">
                                 <h3>Project Info</h3>
-                                <p>Matchmaker is an online email based portal that allows you to ask out your crush with ease, if you know his/her email that is. If you and your crush enter each others emails, you will be notified. Happy crushing <3  </p>
+                                <p>Matchmaker was a fun project, the idea was to make something good and usable for our seniors overnight. It was never a serious project, but it deservers attention considering the number of users we have there ;) </p>
+                                <p>Matchmaker is an online email based portal that allows you to ask out your crush with ease, if you know his/her email that is it. If you and your crush enter each others emails, you will be notified. Happy crushing <3  </p>
                             </div>
                             <div class="live-preview">
                                 <a target="blank" href="http://matchmaker.zense.co.in" class="btn btn-common uppercase">Check It Out!</a>
@@ -353,7 +372,56 @@ $test =2;
                 </div>
             </div>
         </div>
+		
+		<!-- Modal -->
+        <div class="modal fade" id="modal6" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" area-hidden="false">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title" id="myModalLabel">Connect the Disconnected</h4>
+                    </div>
+                    <!--Modal Content-->
+                    <br>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <img src="images/portfolio-details/6.png" class="img-responsive" alt="">
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="project-name overflow">
+                                <h2 class="bold">Connect the Disconnected</h2>
+                                <ul class="nav navbar-nav navbar-default">
+                                    <li><a href="#"><i class="fa fa-clock-o"></i>August,2014</a></li>
+                                </ul>
+                            </div>
+                            <div class="project-info overflow">
+                                <h3>Project Info</h3>
+                                <h3>Connect the Disconnected</h3>
+                                <p>This was the project which won us 2nd Runners Up Award at the IIMB Policy Hackathon. This project was worked on by</p>
+                                <ul>
+									<li>Vikas Yadav</li>
+									<li>Siddartha Sekhar Padhi</li>
+									<li>Aditya Paliwal</li>
+									<li>Shivam Kumar</li>
+                                </ul>
+                                <p>The idea was to crowd source all the government schemes into one place. We are developing this to make it more usable and we'll propose it to Govt of Karnataka later on.</p>
+								<p>Source code available on github <a target=="blank" href="https://github.com/zense/policy_hackathon">Click Here</a></p>
+                            </div>
+                            <div class="live-preview">
+                                <a target="blank" href="http://ctdkarnataka.zense.co.in" class="btn btn-common uppercase">Check It Out!</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!--End of Modal Content-->
+                    <div class="modal-footer">
+                        <button  type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+
+        <!-- Modal -->
 
         <!-- Modal -->
         <div class="modal fade" id="modal5" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" area-hidden="false">
@@ -392,6 +460,16 @@ $test =2;
                 </div>
             </div>
         </div>
+    <script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-53194921-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>    
     <script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/jquery.isotope.min.js"></script>
