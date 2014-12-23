1d36524c061d6bba529729c6de7fa0b0a4b52e88
bobo <vikasyadavgood@gmail.com>
2014-12-24 02:31:18 +0530

    Minor changes added

--- portfolio.php
+++ portfolio.php
@@ -26,7 +26,145 @@ $test =2;
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+   
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<style type="text/css">
+	span.stars, span.stars span {
+		display: block;
+		background: url(images/stars.png) 0 -16px repeat-x;
+		width: 80px;
+		height: 16px;
+	}
+
+	span.stars span {
+		background-position: 0 0;
+	}
+	</style>
+    <script>
+    $(document).ready(function(){
+		$.fn.stars = function() {
+			return $(this).each(function() {
+				$(this).html($('<span />').width(Math.max(0, (Math.min(5, parseFloat($(this).html())))) * 16));
+			});
+		}
+		
+        var get_projects = function(){
+            $.ajax({
+                type: 'GET',
+                url: 'https://dl.dropboxusercontent.com/s/32n0rxhuk6hnal4/projects.json?dl=0',
+                    dataType: 'json',
+                    encode: true
+            })
+
+            .done(function(data){
+                console.log(data);
+                var i=0, j=0;
+                var ap = '<div class="row">'
+                var bp = '<div class="row">'
+                $.each(data.projects, function(key, item){
+					var d= '<a href="'+item.url+'" target="__blank" class="btn btn-common uppercase">Check this out!</a>'
+					$('body').append('<div class="modal fade" id="modal'+item.id+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" area-hidden="false">\
+            <div class="modal-dialog">\
+                <div class="modal-content">\
+                    <div class="modal-header">\
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>\
+                        <h4 class="modal-title" id="myModalLabel">'+item.name+'</h4>\
+                    </div>\
+                    <br>\
+                    <div class="row">\
+                        <div class="col-sm-6">\
+                            <img src="'+item.image+'" class="img-responsive" alt="">\
+                        </div>\
+                        <div class="col-sm-6">\
+                            <div class="project-name overflow">\
+                                <h2 class="bold">'+item.name+'</h2>\
+                                <ul class="nav navbar-nav navbar-default">\
+                                    <li><a href="#"><i class="fa fa-clock-o"></i>July,2014</a></li>\
+                                </ul>\
+                            </div>\
+                            <div class="project-info overflow">\
+                                <h3>Project Info</h3>'+
+                                item.information +
+                            '</div>\
+                            <div class="live-preview">'+d+'\
+                            </div>\
+                        </div>\
+                    </div>\
+                    <div class="modal-footer">\
+                        <button  type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>')
+					if(item.status=="1"){
+						ap += '<div class="col-xs-6 col-sm-4 col-md-3 portfolio-item branded logos">\
+						<span class="stars">'+item.rating+'</span>\
+                        <div class="portfolio-wrapper">\
+                            <div class="portfolio-single">\
+                                <div class="portfolio-thumb" style="width: 100%; height: 200px;position: relative; overflow: hidden">\
+                                    <img src="'+item.image+'" style="width:100% height: auto" class="img-responsive" alt="">\
+                                </div>\
+                                <div class="portfolio-view">\
+                                    <ul class="nav nav-pills">\
+                                        <li><a data-toggle="modal" data-target="#modal'+item.id+'"><i class="fa fa-link"></i></a></li>\
+                                    </ul>\
+                                </div>\
+                            </div>\
+                            <div class="portfolio-info ">\
+                                <h2>'+item.name+'</h2>\
+                            </div>\
+                    </div></div>';
+                        i+=1;
+                        if(i%4==0){
+								ap+='</div>'
+						}
+					}
+					else{
+						bp += '<div class="col-xs-6 col-sm-4 col-md-3 portfolio-item branded logos">\
+						<span class="stars">'+item.rating+'</span>\
+                        <div class="portfolio-wrapper">\
+                            <div class="portfolio-single">\
+                                <div class="portfolio-thumb" style="width: 100%; height: 200px;position: relative; overflow: hidden">\
+                                    <img src="'+item.image+'" style="width:100% height: auto" class="img-responsive" alt="">\
+                                </div>\
+                                <div class="portfolio-view">\
+                                    <ul class="nav nav-pills">\
+                                        <li><a data-toggle="modal" data-target="#modal'+item.id+'"><i class="fa fa-link"></i></a></li>\
+                                    </ul>\
+                                </div>\
+                            </div>\
+                            <div class="portfolio-info ">\
+                                <h2>'+item.name+'</h2>\
+                            </div>\
+                    </div></div>';
+                    
+                        j+=1;
+                        if(j%4==0){
+								bp+='</div>'
+						}
+					}
+					
+                });
+                if(i%4!=0) ap+='</div>'	;
+                if(j%4!=0) bp+='</div>';
+                $('#completed').append(ap)
+                $('#ongoing').append(bp)
+                 $('span.stars').stars();
+            })
+            .fail(function(data){
+                alert("ajax failed");
+                console.log(data);
+            });
+
+        };
+        get_projects();
+        $('span.stars').stars();
+        
+	});
+</script>
+ 
 </head><!--/head-->
+
 
 <body>
 	<header id="header">      
@@ -132,117 +270,7 @@ $test =2;
 	  ga('send', 'pageview');
 
 	</script>    
-    <script type="text/javascript" src="js/jquery.js"></script>
-    <script>
-    $(document).ready(function(){
-        var get_projects = function(){
-            $.ajax({
-                type: 'GET',
-                url: 'https://dl.dropboxusercontent.com/s/32n0rxhuk6hnal4/projects.json?dl=0',
-                    dataType: 'json',
-                    encode: true
-            })
-
-            .done(function(data){
-                console.log(data);
-                var i=0, j=0;
-                var ap = '<div class="row">'
-                var bp = '<div class="row">'
-                $.each(data.projects, function(key, item){
-					var d= '<a href="'+item.url+'" target="__blank" class="btn btn-common uppercase">Check this out!</a>'
-					$('body').append('<div class="modal fade" id="modal'+item.id+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" area-hidden="false">\
-            <div class="modal-dialog">\
-                <div class="modal-content">\
-                    <div class="modal-header">\
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>\
-                        <h4 class="modal-title" id="myModalLabel">'+item.name+'</h4>\
-                    </div>\
-                    <br>\
-                    <div class="row">\
-                        <div class="col-sm-6">\
-                            <img src="'+item.image+'" class="img-responsive" alt="">\
-                        </div>\
-                        <div class="col-sm-6">\
-                            <div class="project-name overflow">\
-                                <h2 class="bold">'+item.name+'</h2>\
-                                <ul class="nav navbar-nav navbar-default">\
-                                    <li><a href="#"><i class="fa fa-clock-o"></i>July,2014</a></li>\
-                                </ul>\
-                            </div>\
-                            <div class="project-info overflow">\
-                                <h3>Project Info</h3>'+
-                                item.information +
-                            '</div>\
-                            <div class="live-preview">'+d+'\
-                            </div>\
-                        </div>\
-                    </div>\
-                    <div class="modal-footer">\
-                        <button  type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
-                    </div>\
-                </div>\
-            </div>\
-        </div>')
-					if(item.status=="1"){
-						ap += '<div class="col-xs-6 col-sm-4 col-md-3 portfolio-item branded logos">\
-                        <div class="portfolio-wrapper">\
-                            <div class="portfolio-single">\
-                                <div class="portfolio-thumb" style="width: 100%; height: 200px;position: relative; overflow: hidden">\
-                                    <img src="'+item.image+'" style="width:100% height: auto" class="img-responsive" alt="">\
-                                </div>\
-                                <div class="portfolio-view">\
-                                    <ul class="nav nav-pills">\
-                                        <li><a data-toggle="modal" data-target="#modal'+item.id+'"><i class="fa fa-link"></i></a></li>\
-                                    </ul>\
-                                </div>\
-                            </div>\
-                            <div class="portfolio-info ">\
-                                <h2>'+item.name+'</h2>\
-                            </div>\
-                    </div></div>';
-                        i+=1;
-                        if(i%4==0){
-								ap+='</div>'
-						}
-					}
-					else{
-						bp += '<div class="col-xs-6 col-sm-4 col-md-3 portfolio-item branded logos">\
-                        <div class="portfolio-wrapper">\
-                            <div class="portfolio-single">\
-                                <div class="portfolio-thumb" style="width: 100%; height: 200px;position: relative; overflow: hidden">\
-                                    <img src="'+item.image+'" style="width:100% height: auto" class="img-responsive" alt="">\
-                                </div>\
-                                <div class="portfolio-view">\
-                                    <ul class="nav nav-pills">\
-                                        <li><a data-toggle="modal" data-target="#modal'+item.id+'"><i class="fa fa-link"></i></a></li>\
-                                    </ul>\
-                                </div>\
-                            </div>\
-                            <div class="portfolio-info ">\
-                                <h2>'+item.name+'</h2>\
-                            </div>\
-                    </div></div>';
-                        j+=1;
-                        if(j%4==0){
-								bp+='</div>'
-						}
-					}
-					
-                });
-                if(i%4!=0) ap+='</div>'	;
-                if(j%4!=0) bp+='</div>';
-                $('#completed').append(ap)
-                $('#ongoing').append(bp)
-            })
-            .fail(function(data){
-                alert("ajax failed");
-                console.log(data);
-            });
-
-        };
-        get_projects()
-	});
-</script>
+    
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/jquery.isotope.min.js"></script>
     <script type="text/javascript" src="js/lightbox.min.js"></script>
